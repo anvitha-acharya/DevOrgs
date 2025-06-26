d7cc118fa7cf2ec27b5458f3565f711b3a4ee0c2
anvitha-acharya <anvitha.acharya2k4@gmail.com>
2025-06-26 22:33:31 +0530

    Fixed render build fail

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -31,6 +31,7 @@ export function LandingPage() {
   }, []);
 
   const handleSubmit = async (e: React.FormEvent) => {
+  const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000' ;
     e.preventDefault();
 
     if (!isLogin) {
@@ -41,7 +42,7 @@ export function LandingPage() {
       }
 
       try {
-  const res = await axios.post<SignupResponse>('http://localhost:5000/api/auth/signup', formData);
+  const res = await axios.post<SignupResponse>('${API_BASE_URL}/api/auth/signup', formData);
         alert(res.data.message);
       } catch (err: unknown) {
   if (axios.isAxiosError(err)) {
